554719d76900fb29bdf757b26f4706b97e142ba9
noah <noahskorner@gmail.com>
2023-10-31 20:11:33 -0500

    Fixed lint errors.

--- packages/ui/components/button/button.tsx
+++ packages/ui/components/button/button.tsx
@@ -1,14 +1,4 @@
-import { useEffect, useState } from 'react';
-
 const Button = () => {
-  const [state] = useState(false);
-
-  useEffect(() => {
-    if (state == true) {
-      console.log('false');
-    }
-  }, []);
-
   return <button>Click me!</button>;
 };
 
