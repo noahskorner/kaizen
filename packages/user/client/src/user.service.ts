c9e4d855ed4c3115d308e59af8d20fb46fd63340
noah <noahskorner@gmail.com>
2023-11-20 16:55:55 -0600

    Fixes build errors

--- packages/user/client/src/user.service.ts
+++ packages/user/client/src/user.service.ts
@@ -1,6 +1,5 @@
-import { User } from '@kaizen/core';
 import { apiClient } from '@kaizen/ui';
-import { CreateUserRequest } from '@kaizen/user';
+import { CreateUserRequest, User } from '@kaizen/user';
 
 export const UserService = {
   create: (request: CreateUserRequest) => apiClient.post<User>('/user', request)
