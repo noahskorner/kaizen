09845dbb58ec87b143bfc09497716763a2e705ba
noah <noahskorner@gmail.com>
2023-11-20 12:49:04 -0600

    Updates vite.config

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,5 +1,10 @@
 import './globals.css';
+import { TextInput } from '@kaizen/ui';
 
 export default function App() {
-  return <div className="h-screen w-screen bg-slate-900"></div>;
+  return (
+    <div className="h-screen w-screen bg-slate-900">
+      <TextInput id={''} label={''} value={''} onChange={() => {}} />
+    </div>
+  );
 }
