de997777f58463cdb3a3f700d236a3412af085d3
noah <noahskorner@gmail.com>
2023-11-21 15:09:08 -0600

    Adds logout feature.

--- apps/frontend/src/pages/routes.tsx
+++ apps/frontend/src/pages/routes.tsx
@@ -1,10 +1,14 @@
-import { RouteObject } from 'react-router-dom';
 import { HomePage } from './home/home.page';
 import { LoginPage } from './login/login.page';
 import { RegisterPage } from './register/register.page';
+import { DashboardPage } from './dashboard/dashboard.page';
 
-export const routes: Record<string, RouteObject> = {
+export const routes: Record<
+  string,
+  { path: string; element: React.ReactNode }
+> = {
   home: { path: '/', element: <HomePage /> },
   login: { path: '/login', element: <LoginPage /> },
-  register: { path: '/register', element: <RegisterPage /> }
+  register: { path: '/register', element: <RegisterPage /> },
+  dashboard: { path: '/dashboard', element: <DashboardPage /> }
 };
