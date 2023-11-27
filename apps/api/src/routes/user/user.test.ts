65aee3fff4b64a4dbd71f5eb8aa3ea9602d6e341
Noah Korner <noah.korner@rsmus.com>
2023-11-27 12:41:56 -0600

    Adds tests for createLinkToken.

--- apps/api/src/routes/user/user.test.ts
+++ apps/api/src/routes/user/user.test.ts
@@ -2,128 +2,155 @@ import { ErrorKey } from '@kaizen/core';
 import supertest from 'supertest';
 import { app } from '../../app';
 import { createUniqueEmail } from '../../fixtures/create-unique-email';
+import { createAndLoginUser } from '../../fixtures/create-and-login-user';
 import { v4 as uuid } from 'uuid';
 import { expectError } from '../../fixtures/expect-error';
 import { validPassword } from '../../fixtures/valid-password';
 import { CreateUserCommand } from '@kaizen/user-server';
 import { User } from '@kaizen/user';
+import { LinkToken } from '@kaizen/user/src/link-token';
 
-describe('/user should', () => {
-  it('returns 400 when request is null', async () => {
-    // Act
-    const response = await supertest(app).post('/user').send();
+describe('/user', () => {
+  describe('create should', () => {
+    it('returns 400 when request is null', async () => {
+      // Act
+      const response = await supertest(app).post('/user').send();
 
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_INVALID_EMAIL);
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_INVALID_EMAIL);
+    });
+    it('returns 400 when request is empty', async () => {
+      // Arrange
+      const request = {};
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_INVALID_EMAIL);
+    });
+    it('returns 400 when email is not valid', async () => {
+      // Arrange
+      const request = {
+        email: 'invalid-email'
+      };
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_INVALID_EMAIL);
+    });
+    it('returns 400 when password not provided', async () => {
+      // Arrange
+      const request = {
+        email: createUniqueEmail()
+      };
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_INVALID_PASSWORD);
+    });
+    it('returns 400 when password not long enough', async () => {
+      // Arrange
+      const request: CreateUserCommand = {
+        email: createUniqueEmail(),
+        password: '1234567'
+      };
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_PASSWORD_TOO_SHORT);
+    });
+    it('returns 400 when password has no numbers', async () => {
+      // Arrange
+      const request: CreateUserCommand = {
+        email: createUniqueEmail(),
+        password: 'abcdefghi'
+      };
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_PASSWORD_NO_NUMBER);
+    });
+    it('returns 400 when password has no symbols', async () => {
+      // Arrange
+      const request: CreateUserCommand = {
+        email: createUniqueEmail(),
+        password: '1234567a'
+      };
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_PASSWORD_NO_SYMBOL);
+    });
+    it('returns 400 when email already exists', async () => {
+      // Arrange
+      const request: CreateUserCommand = {
+        email: createUniqueEmail(),
+        password: validPassword
+      };
+      await supertest(app).post('/user').send(request);
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expectError(response, ErrorKey.CREATE_USER_EMAIL_ALREADY_EXISTS);
+    });
+    it('returns 201 and user with normalized email', async () => {
+      // Arrange
+      const request: CreateUserCommand = {
+        email: `${uuid()}-UPPERcase@test.com`,
+        password: validPassword
+      };
+
+      // Act
+      const response = await supertest(app).post('/user').send(request);
+      const body: User = response.body;
+
+      // Assert
+      expect(response.statusCode).toBe(201);
+      expect(body.email).toBe(request.email.toLowerCase());
+    });
   });
-  it('returns 400 when request is empty', async () => {
-    // Arrange
-    const request = {};
+  describe('createLinkToken should', () => {
+    it('return 401 when user is not authenticated', async () => {
+      // Act
+      const response = await supertest(app).post('/user/link-token').send();
 
-    // Act
-    const response = await supertest(app).post('/user').send(request);
+      // Assert
+      expect(response.statusCode).toBe(401);
+    });
+    it('returns 201 and link token', async () => {
+      // Arrange
+      const { accessToken } = await createAndLoginUser();
 
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_INVALID_EMAIL);
-  });
-  it('returns 400 when email is not valid', async () => {
-    // Arrange
-    const request = {
-      email: 'invalid-email'
-    };
+      // Act
+      const response = await supertest(app)
+        .post('/user/link-token')
+        .auth(accessToken, { type: 'bearer' });
+      const linkToken = response.body as LinkToken;
 
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_INVALID_EMAIL);
-  });
-  it('returns 400 when password not provided', async () => {
-    // Arrange
-    const request = {
-      email: createUniqueEmail()
-    };
-
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_INVALID_PASSWORD);
-  });
-  it('returns 400 when password not long enough', async () => {
-    // Arrange
-    const request: CreateUserCommand = {
-      email: createUniqueEmail(),
-      password: '1234567'
-    };
-
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_PASSWORD_TOO_SHORT);
-  });
-  it('returns 400 when password has no numbers', async () => {
-    // Arrange
-    const request: CreateUserCommand = {
-      email: createUniqueEmail(),
-      password: 'abcdefghi'
-    };
-
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_PASSWORD_NO_NUMBER);
-  });
-  it('returns 400 when password has no symbols', async () => {
-    // Arrange
-    const request: CreateUserCommand = {
-      email: createUniqueEmail(),
-      password: '1234567a'
-    };
-
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_PASSWORD_NO_SYMBOL);
-  });
-  it('returns 400 when email already exists', async () => {
-    // Arrange
-    const request: CreateUserCommand = {
-      email: createUniqueEmail(),
-      password: validPassword
-    };
-    await supertest(app).post('/user').send(request);
-
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-
-    // Assert
-    expect(response.statusCode).toBe(400);
-    expectError(response, ErrorKey.CREATE_USER_EMAIL_ALREADY_EXISTS);
-  });
-  it('returns 201 and user with normalized email', async () => {
-    // Arrange
-    const request: CreateUserCommand = {
-      email: `${uuid()}-UPPERcase@test.com`,
-      password: validPassword
-    };
-
-    // Act
-    const response = await supertest(app).post('/user').send(request);
-    const body: User = response.body;
-
-    // Assert
-    expect(response.statusCode).toBe(201);
-    expect(body.email).toBe(request.email.toLowerCase());
+      // Assert
+      expect(response.statusCode).toBe(201);
+      expect(linkToken.token).toBeDefined();
+    });
   });
 });
--- packages/user/shared/src/index.ts
+++ packages/user/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from './create-user.request';
 export * from './create-user.validator';
 export * from './create-link-token.request';
 export * from './user';
+export * from './link-token';
