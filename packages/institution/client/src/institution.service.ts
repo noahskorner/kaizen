b6918f30a0078b1cf61d42643981b975b88e22ed
Noah Korner <noah.korner@rsmus.com>
2023-12-06 10:09:51 -0600

    Fixes typo

--- packages/institution/client/src/institution.service.ts
+++ packages/institution/client/src/institution.service.ts
@@ -1,7 +1,7 @@
 import { apiClient } from '@kaizen/ui';
 import { CreateInstitutionRequest, Institution } from '@kaizen/institution';
 
-export const InstituionService = {
+export const InstitutionService = {
   create: (request: CreateInstitutionRequest) =>
     apiClient.post<Institution>('/institution', request),
   find: () => apiClient.get<Institution[]>('/institution')
