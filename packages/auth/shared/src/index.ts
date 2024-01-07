4fcccdec60a1fc4da5ffa81e32c9293198c0f81a
noah <noahskorner@gmail.com>
2024-01-07 13:04:56 -0600

    Let's try tsc again...

--- packages/auth/shared/src/index.ts
+++ packages/auth/shared/src/index.ts
@@ -1,5 +1,5 @@
 export * from './login';
-export * from './refresh-token/index';
 export * from './access-token';
 export * from './auth-token';
 export * from './refresh-token';
+export * from './refresh-token/index';
