a36da9f9146fb8970f66a8d76dc9c4dde9d15444
Noah Korner <noah.korner@rsmus.com>
2024-03-15 15:45:53 -0500

    feature/event-bus: Fixes failing tests.

--- apps/api/test/get-random-enum.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getRandomEnum = (enumeration: any): any => {
-  const values = Object.values(enumeration);
-  const randomIndex = Math.floor(Math.random() * values.length);
-  return values[randomIndex];
-};
