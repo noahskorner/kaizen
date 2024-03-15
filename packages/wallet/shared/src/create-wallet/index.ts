2635fca933530c50fb18b453561ab16ef4845160
noah <noahskorner@gmail.com>
2024-03-15 18:03:42 -0500

    feature/wallet: Added feature to create wallet

--- packages/wallet/shared/src/create-wallet/index.ts
+++ packages/wallet/shared/src/create-wallet/index.ts
@@ -2,3 +2,4 @@ export * from './create-wallet.command';
 export * from './create-wallet.query';
 export * from './create-wallet.repository.interface';
 export * from './create-wallet.service.interface';
+export * from './create-wallet.validator';
---
+++ packages/wallet/shared/src/get-wallet/index.ts
@@ -0,0 +1,2 @@
+export * from './get-wallet-by-user-id.query';
+export * from './get-wallet.repository.interface';
