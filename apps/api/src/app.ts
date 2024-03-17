af01e402128b649e597b6e420022f68df2ac33a1
noah <noahskorner@gmail.com>
2024-03-17 14:01:36 -0500

    KAI-8: Gives user 10 coins upon login.

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,8 @@
-import { CreateUserSuccessEvent, ServiceEventType } from '@kaizen/core-server';
+import { ServiceEvent, ServiceEventType } from '@kaizen/core-server';
 import { AppBuilder } from './app-builder';
 import { ServiceCollectionBuilder } from './service-collection.builder';
+import { UpdateWalletCommand } from '@kaizen/wallet';
+import { v4 as uuid } from 'uuid';
 
 const serviceCollection = new ServiceCollectionBuilder().build();
 
@@ -9,8 +11,19 @@ const appBuilder = new AppBuilder()
   .withListeners({
     [ServiceEventType.CREATE_USER_SUCCESS]: [
       // When a user is created, create a wallet for them
-      (event: CreateUserSuccessEvent) =>
+      (event: ServiceEvent) =>
         serviceCollection.createWalletService.create(event.payload)
+    ],
+    [ServiceEventType.LOGIN_SUCCESS]: [
+      // When a user logs in, give them 10 coins
+      (event: ServiceEvent) => {
+        const command: UpdateWalletCommand = {
+          userId: event.payload.userId,
+          transactionId: uuid(),
+          amount: 10
+        };
+        serviceCollection.updateWalletService.update(command);
+      }
     ]
   });
 
