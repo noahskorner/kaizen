646fa0af8c5971887dd0d1665042eb08a8aa07d2
noah <noahskorner@gmail.com>
2024-03-17 15:36:38 -0500

    KAI-18: Adds contact us form.

--- apps/frontend/src/pages/landing/landing.page.tsx
+++ apps/frontend/src/pages/landing/landing.page.tsx
@@ -1,10 +1,37 @@
+import { useState, FormEvent } from 'react';
 import './landing.page.css';
-import content from './content.json';
 
 export const LandingPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onNameChange = (event: FormEvent<HTMLInputElement>) => {
+    setName(event.currentTarget.value);
+  };
+
+  const onEmailChange = (event: FormEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onMessageChange = (event: FormEvent<HTMLTextAreaElement>) => {
+    setMessage(event.currentTarget.value);
+  };
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const contact = {
+      name,
+      email,
+      message
+    };
+    console.log({ contact });
+  };
+
   return (
     <div>
-      <div className="flex h-[90vh] w-full flex-col items-center bg-circle-gradient from-blue-600 to-slate-900">
+      <div className="bg-circle-gradient flex min-h-screen w-full flex-col items-center from-blue-600 to-slate-900">
         <nav className="flex h-36 w-full items-center justify-between bg-gradient-to-b from-slate-950 via-blue-950 to-transparent p-8">
           <div className="w-1/3"></div>
           <div className="hidden lg:flex">
@@ -23,26 +50,96 @@ export const LandingPage = () => {
           </div>
           <div className="w-1/3"></div>
         </nav>
-        <div className="flex h-full w-full flex-col-reverse p-4 md:flex-row">
-          <div className="flex h-full w-full flex-col items-end justify-center">
-            <div className="flex w-full max-w-4xl flex-col gap-y-8">
-              <h1 className="font-secondary text-3xl font-semibold leading-tight text-white md:text-4xl md:leading-tight xl:text-5xl xl:leading-tight">
-                Empowering Innovation through Human-Centered Design, Test-Driven
-                Development, and Efficient Feature Delivery.
+        <div className="flex w-full flex-grow flex-col items-center gap-y-16 px-4 pb-32 2xl:flex-row 2xl:px-16">
+          <div className="flex w-full flex-col items-center justify-center 2xl:items-end">
+            <div className="flex w-full flex-col gap-y-8">
+              <h1 className="font-secondary text-3xl font-semibold leading-snug text-white 2xl:text-5xl 2xl:leading-snug">
+                Turbocharge your business with our help.
               </h1>
-              <p className="text-gray-300">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Dolorum necessitatibus, vel dolorem quo distinctio, dicta
-                recusandae hic molestiae, magni nobis aliquid quisquam voluptas
-                laborum expedita esse sunt! Sapiente, voluptas adipisci?
+              <p className="text-lg text-gray-300">
+                We prioritize&nbsp;
+                <span className="underline">Human-Driven Design</span>,&nbsp;
+                <span className="underline">Test-Driven Development</span>,
+                and&nbsp;
+                <span className="underline">
+                  Impactful AI and Machine Learning
+                </span>
+                &nbsp;solutions. We specialize in&nbsp;
+                <span className="underline">Vertical Slice Architecture</span>,
+                ensuring efficient and scalable systems. With a focus on &nbsp;
+                <span className="underline">
+                  Continuous Integration/Continuous Deployment
+                </span>
+                &nbsp;and&nbsp;
+                <span className="underline">Infrastructure as Code</span>, we
+                deliver cutting-edge solutions tailored to your business needs.
               </p>
             </div>
           </div>
-          <div className="flex h-full w-full items-center justify-center"></div>
+          <div className="flex w-full items-center justify-center">
+            <div className="w-full max-w-xl rounded bg-slate-50 p-8 shadow-lg">
+              <form onSubmit={onSubmit} className="flex w-full flex-col">
+                <h2 className="mb-8 font-secondary text-3xl font-semibold leading-snug text-gray-900">
+                  How can we help?
+                </h2>
+                <div className="mb-5">
+                  <label
+                    htmlFor="name"
+                    className="mb-2 block text-sm font-medium text-gray-900">
+                    Your name
+                  </label>
+                  <input
+                    value={name}
+                    onInput={onNameChange}
+                    type="text"
+                    id="name"
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                    placeholder="Jon Snow"
+                    required
+                  />
+                </div>
+                <div className="mb-5">
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium text-gray-900 ">
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                    required
+                    placeholder="jon.snow@gmail.com"
+                    value={email}
+                    onInput={onEmailChange}
+                  />
+                </div>
+                <div className="mb-5">
+                  <label
+                    htmlFor="message"
+                    className="mb-2 block text-sm font-medium text-gray-900">
+                    Your message
+                  </label>
+                  <textarea
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+                    placeholder="How can we help?"
+                    required
+                    value={message}
+                    onInput={onMessageChange}></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="h-[7vh] w-full bg-white"></div>
-      <div className="content">
+      {/* <div className="content">
         {content.map(({ title, subtitle }, index) => {
           return (
             <div key={index} className="content__item">
@@ -59,7 +156,7 @@ export const LandingPage = () => {
             </div>
           );
         })}
-      </div>
+      </div> */}
       {/* <footer className="h-[20vh] w-full bg-gradient-to-r from-slate-900 to-blue-600"></footer> */}
     </div>
   );
