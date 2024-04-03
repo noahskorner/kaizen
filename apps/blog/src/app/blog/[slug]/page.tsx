9024e334925240bc9f7a68d16809622ecb8b0580
noah <noahskorner@gmail.com>
2024-04-02 19:05:19 -0500

    KAI-30: Fixes build errors

--- apps/blog/src/app/blog/[slug]/page.tsx
+++ apps/blog/src/app/blog/[slug]/page.tsx
@@ -8,7 +8,7 @@ const ARTICLE_DIRECTORY = path.join(process.cwd(), 'src', 'articles');
 
 export default function Article({ params }: { params: { slug: string } }) {
   const articlePath = path.join(ARTICLE_DIRECTORY, params.slug + '.md');
-  const { content, data } = matter(fs.readFileSync(articlePath, 'utf8'));
+  const { content } = matter(fs.readFileSync(articlePath, 'utf8'));
 
   return (
     <div className="flex w-full justify-center p-8">
