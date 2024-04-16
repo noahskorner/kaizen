85097a0f09b90350fb3cb56245ff69e3403a2428
noah <noahskorner@gmail.com>
2024-04-16 18:27:16 -0500

    KAI-42: Adds happy path case.

--- packages/finance/server/src/expense/find/find-expenses.repository.ts
+++ packages/finance/server/src/expense/find/find-expenses.repository.ts
@@ -8,9 +8,13 @@ export class FindExpensesRepository
 {
   public async find(query: FindExpensesQuery): Promise<ExpenseRecord[]> {
     const expenses = await this._prisma.$queryRaw`
-        SELECT * FROM transaction
-        WHERE user_id = {0}
-        GROUP BY 
+      SELECT "category"."primary" as "category", SUM("transaction"."amount") as "amount"
+      FROM "category"
+      JOIN "transaction" on "transaction"."category_id" = "category"."id"
+      WHERE "transaction"."user_id" = ${query.userId}
+      GROUP BY "category"."primary";
     `;
+
+    return expenses as ExpenseRecord[];
   }
 }
