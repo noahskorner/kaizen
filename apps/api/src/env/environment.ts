b9f0a1a7a6bea47b8763dd732935bbe6e7011b9a
Noah Korner <noahskorner@gmail.com>
2024-05-15 16:34:36 -0500

    KAI-40: Splits environment into interface file. (#97)

--- apps/api/src/env/environment.ts
+++ apps/api/src/env/environment.ts
@@ -1,16 +1,4 @@
-export interface Environment extends Record<string, string> {
-  NODE_ENV: 'DEVELOPMENT' | 'TEST' | 'PRODUCTION';
-  ACCESS_TOKEN_SECRET: string;
-  ACCESS_TOKEN_EXPIRATION: string;
-  REFRESH_TOKEN_SECRET: string;
-  REFRESH_TOKEN_EXPIRATION: string;
-  REFRESH_TOKEN_COOKIE_DOMAIN: string;
-  API_PORT: string;
-  API_DOMAIN: string;
-  FRONTEND_DOMAIN: string;
-  PLAID_CLIENT_ID: string;
-  PLAID_SECRET: string;
-}
+import { Environment } from './environment.interface';
 
 const NODE_ENV = process.env.NODE_ENV ?? '';
 if (
