fbae98a21d169596fe4eca4669d8439596d72010
Noah Korner <noahskorner@gmail.com>
2024-07-19 09:36:46 -0500

    feature/deploy: Deploys api to ecs. (#108)

--- infrastructure/src/api.ts
+++ infrastructure/src/api.ts
@@ -2,21 +2,25 @@ import * as cdk from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as logs from 'aws-cdk-lib/aws-logs';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 import { config } from './config';
 import { environment } from '../../apps/api/src/env/environment';
 
 export interface ApiStackProps {
   vpc: ec2.Vpc;
-  databaseSecurityGroup: ec2.SecurityGroup;
+  apiSecurityGroup: ec2.SecurityGroup;
+  databaseSecret: secretsmanager.ISecret;
 }
 
 export class ApiStack extends cdk.Stack {
   constructor(
     scope: Construct,
     id: string,
-    { vpc, databaseSecurityGroup }: ApiStackProps
+    { vpc, apiSecurityGroup, databaseSecret }: ApiStackProps
   ) {
     super(scope, id);
 
@@ -32,6 +36,13 @@ export class ApiStack extends cdk.Stack {
       vpc: vpc
     });
 
+    // Create a log group
+    // TODO: Move to config
+    const logGroup = new logs.LogGroup(this, 'kaizen-ecs-container', {
+      logGroupName: 'kaizen-ecs-container',
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
     // Create a task definition with the container
     const taskDefinition = new ecs.FargateTaskDefinition(
       this,
@@ -41,30 +52,38 @@ export class ApiStack extends cdk.Stack {
         cpu: 256
       }
     );
+
+    // Allow the task to access the database secret
+    const secretAccessPolicyStatement = new iam.PolicyStatement({
+      actions: ['secretsmanager:GetSecretValue'],
+      resources: [databaseSecret.secretArn]
+    });
+    taskDefinition.addToTaskRolePolicy(secretAccessPolicyStatement);
+
+    // Create the task definition
+    // TODO: Fix this environment bullshit
+    const ecsEnvironment = {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ...(environment as any)
+    };
+    if (ecsEnvironment.AWS_DATABASE_WHITELIST != null) {
+      delete ecsEnvironment.AWS_DATABASE_WHITELIST;
+    }
     taskDefinition.addContainer(config.API_CONTAINER_ID, {
       image: ecs.ContainerImage.fromEcrRepository(repository),
-      environment: {
-        ...environment,
-        // TODO: Maybe we shouldn't set the environment here...
-        AWS_DATABASE_WHITELIST: ''
-      } as unknown as Record<string, string>,
+      environment: ecsEnvironment,
       portMappings: [
         {
           containerPort: 3001
         }
-      ]
+      ],
+      logging: ecs.LogDrivers.awsLogs({
+        logGroup: logGroup,
+        streamPrefix: 'kaizen-ecs-container'
+      })
     });
 
-    // Create a security group for the EC2 instance
-    const apiSecurityGroup = new ec2.SecurityGroup(
-      this,
-      config.API_SECURITY_GROUP_ID,
-      { vpc: vpc }
-    );
-    apiSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
-    apiSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
-
-    // Create a t3.micro EC2 instance and add it to the cluster
+    // Create a t3.micro ec2 instance and add it to the cluster
     cluster.addCapacity(config.API_SCALING_GROUP_CAPACITY_ID, {
       vpcSubnets: {
         // TODO: Making this public to save on NAT Gateway costs
@@ -78,9 +97,8 @@ export class ApiStack extends cdk.Stack {
       machineImage: ecs.EcsOptimizedImage.amazonLinux2()
     });
     cluster.connections.addSecurityGroup(apiSecurityGroup);
-    cluster.connections.addSecurityGroup(databaseSecurityGroup);
 
-    // Create a Fargate service
+    // Create a fargate service
     const fargateService = new ecs.FargateService(this, config.API_SERVICE_ID, {
       cluster,
       taskDefinition,
