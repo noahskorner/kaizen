a66f3d7c4436debdfb4267296425f21719083704
Noah Korner <noah.korner@rsmus.com>
2024-07-19 10:13:46 -0500

    Refactor update email into UpdateEmailForm.

--- packages/user/client/src/index.ts
+++ packages/user/client/src/index.ts
@@ -1,4 +1,5 @@
 export * from './create-user-form';
+export * from './update-email-form';
 export * from './update-email.client';
 export * from './user.client';
 export * from './verify-update-email.client';
