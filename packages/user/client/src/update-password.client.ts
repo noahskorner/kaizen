e74f7a42a23fa512d35de05b4de10e5dacb923f3
noah <noahskorner@gmail.com>
2024-07-20 12:41:48 -0500

    Fixes lint errors.

--- packages/user/client/src/update-password.client.ts
+++ packages/user/client/src/update-password.client.ts
@@ -5,7 +5,10 @@ import { UpdatePasswordRequest } from '@kaizen/user';
 export const UpdatePasswordClient = {
   update: (request: UpdatePasswordRequest): Promise<ApiResponse<boolean>> => {
     return handleAxiosRequest(() => {
-      return ApiClient.post<ApiResponse<boolean>>(`/user/password/token`, request);
+      return ApiClient.post<ApiResponse<boolean>>(
+        `/user/password/token`,
+        request
+      );
     });
   }
 };
